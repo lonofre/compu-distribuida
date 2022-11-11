10bfe3ab2a2b1dc1fb0e3b1e1e2102795ebd9cc3
victoriaebaronh <72566279+victoriaebaronh@users.noreply.github.com>
2022-11-10 18:58:56 -0600

    Update bob.ex

--- mutual_exclusion/lib/bob.ex
+++ mutual_exclusion/lib/bob.ex
@@ -1,7 +1,41 @@
-defmodule Bob do
+defmodule Alice do
 
-    def start() do
-        
+    def start(idBuzonAlice,idPan) do
+        spawn(fn -> loop(idAlice,idPan)
+          end
+        )
     end
+
+    defp loopAlice(idBuzoAlice,idPan) do
+        flag = false
+        flag2 = false
+        send(:acquire1,idAlice)
+
+        receive do
+          {:acquire1} -> send(idBuzonBob,:acquire1)
+
+          {:acquire2} -> send(idBuzonBob,:acquire2)
+
+          {:release1} -> send(idBuzonBob,:release1)
+
+
+          {:release2} -> send(idBuzonBob,:release2)
+
+        end
+
+        if          do
+
+        else
+
+        end
+
+
+       #Aqui va lo del While #
+
+       #Aqui va lo del pan #
+
+       send (idBuzonAlice, :realease1)
+
+       loopAlice(idBuzonAlice,idPan)
 
 end
